9e33b28ccfcb48adf6dc8b72f27720471cb6170c
Stiders <tomasceatoleii1998@gmail.com>
2020-11-03 16:06:33 -0300

    second commit

---
+++ webempresa/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+
+urlpatterns = [
+	#paths del core
+	path('', include('core.urls')),
+	#paths del services
+	path('services/', include('services.urls')),
+	#paths del services
+	path('blog/', include('blog.urls')),
+	#paths del admin
+	path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+	from django.conf.urls.static import static
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
